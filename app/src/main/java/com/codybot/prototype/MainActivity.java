fc92551f17e8f0e5326fbc86b4c39af61083be2e
Ludar666 <plasma89@hotmail.it>
2026-09-23 05:48:11 +0200

    Fix crash MainActivity all'avvio

--- app/src/main/java/com/codybot/prototype/MainActivity.java
+++ app/src/main/java/com/codybot/prototype/MainActivity.java
@@ -3,24 +3,34 @@ package com.codybot.prototype;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setPadding(50, 50, 50, 50);
+
         Button btnToggle = new Button(this);
         btnToggle.setText("START / STOP");
-        btnToggle.setTextSize(20);
+        btnToggle.setTextSize(18);
 
-        btnToggle.setOnClickListener(v -> {
-            Intent serviceIntent = new Intent(this, ScreenCaptureService.class);
-            serviceIntent.setAction(ScreenCaptureService.ACTION_TOGGLE);
-            startService(serviceIntent);
+        btnToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent serviceIntent = new Intent(MainActivity.this, ScreenCaptureService.class);
+                serviceIntent.setAction(ScreenCaptureService.ACTION_TOGGLE);
+                startService(serviceIntent);
+            }
         });
 
-        setContentView(btnToggle);
+        layout.addView(btnToggle);
+        setContentView(layout);
     }
 }
